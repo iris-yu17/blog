cbbc8bc5ad7833c69f9c77127ad776db5ce14f41
Iris Yu <iris.yu0716@gmail.com>
2024-05-13 17:43:39 +0800

    chore: update article Prose nad image

--- src/app/article/[slug]/page.tsx
+++ src/app/article/[slug]/page.tsx
@@ -11,7 +11,7 @@ export default async function Article({ params }) {
   const filename = articles.find((item) => item.id === slug).name;
   const encodedFileName = encodeURIComponent(filename);
   const res = await fetch(
-    `${process.env.NEXT_PUBLIC_HOST}/markdown/${encodedFileName}`,
+    `${process.env.NEXT_PUBLIC_HOST}/markdown/${encodedFileName}.md`,
   );
 
   const data = await res.text();
@@ -23,9 +23,7 @@ export default async function Article({ params }) {
         <span>Back to list</span>
       </button>
       <div className="my-6 border-b border-solid border-border">
-        <h1 className="text-4xl font-semibold leading-normal">
-          [教學] 用 Github Actions 在 Google Cloud Run 上部署 Next.js
-        </h1>
+        <h1 className="text-4xl font-semibold leading-normal">{filename}</h1>
         <div className="py-4 text-xs text-tertiary">2023/12/30</div>
       </div>
       <div className="flex flex-col gap-16">
--- src/data/article.ts
+++ src/data/article.ts
@@ -3,21 +3,21 @@ import { Category } from '@/types/enum/category';
 const articles = [
   {
     id: 'gcp-get-bucket-obj',
-    name: '[GCP] 使用用戶端程式庫來取得 Bucket 內的物件.md',
+    name: '[GCP] 使用用戶端程式庫來取得 Bucket 內的物件',
     description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam deserunt asperiores, unde corporis perspiciatis cum placeat accusamus commodi nulla maxime iste.Itaque fuga assumenda qui.Maiores abaccusantium porro',
     tags: [Category.Gcp],
     updated: '2024-05-06',
   },
   {
     id: 'pixi-notes',
-    name: '[PIXI.js] v7 基礎筆記.md',
+    name: '[PIXI.js] v7 基礎筆記',
     description: '測試測試測試',
     tags: [Category.Pixi],
     updated: '2024-05-06',
   },
   {
     id: 'accessibile-web-development',
-    name: '[無障礙] 網頁開發筆記.md',
+    name: '[無障礙] 網頁開發筆記',
     description: '網頁開發筆記',
     tags: [Category.Pixi],
     updated: '2024-05-10',
