98852ee17afefa550ada6a7c6858a9a43506001b
Iris Yu <iris.yu0716@gmail.com>
2024-06-03 17:21:32 +0800

    chore: update style

--- src/components/breadcrumb/index.tsx
+++ src/components/breadcrumb/index.tsx
@@ -11,7 +11,7 @@ type BreadCrumbItem = {
 
 export default function BreadCrumb({ items }: { items: BreadCrumbItem[] }) {
   return (
-    <div className="sticky left-0 top-10 -ms-4 mb-4 flex w-[calc(100%+2rem)] flex-wrap items-center gap-1 bg-black-200 px-2 py-1 text-sm font-light text-gray-100 shadow-lg shadow-[#dddddd] dark:shadow-[#131313] md:w-[calc(100%+3rem)] md:-ms-6">
+    <div className="sticky left-0 top-10 -ms-4 mb-4 flex w-[calc(100%+2rem)] flex-wrap items-center gap-1 bg-black-200 px-2 py-1 text-sm text-gray-100 shadow-lg shadow-[#dddddd] md:-ms-6 md:w-[calc(100%+3rem)] dark:font-light dark:shadow-[#131313]">
       <div>
         <span className="text-primary">IRIS</span> Code
       </div>
@@ -23,7 +23,7 @@ export default function BreadCrumb({ items }: { items: BreadCrumbItem[] }) {
           <div key={index} className="flex items-center gap-1">
             <Link
               href={href || PageUrls[key]}
-              className="hover:font-normal hover:text-white-default"
+              className="hover:text-white-default hover:font-medium dark:hover:font-normal"
               scroll={false}
             >
               {text || BreadcrumbText[key]}
