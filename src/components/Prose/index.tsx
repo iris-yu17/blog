5135d65b6e4ea0acc9e48fe952d57d104f8446b8
Iris Yu <iris.yu0716@gmail.com>
2024-06-03 17:37:06 +0800

    chore: update style

--- src/components/Prose/index.tsx
+++ src/components/Prose/index.tsx
@@ -6,7 +6,7 @@ type propsType = {
 
 export default function Prose(props: propsType) {
   return (
-    <div className="prose font-rbtm prose-h2:border-b prose-h2:border-border prose-hr:border-dashed">
+    <div className="prose font-rbtm prose-h2:border-b prose-h2:border-border prose-hr:border-dashed prose-hr:border-gray-200">
       {props.children}
     </div>
   );
