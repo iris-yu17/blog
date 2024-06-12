694fcca21bd262256efb8da2b9edee0bc8c43c95
Iris Yu <iris.yu0716@gmail.com>
2024-06-12 16:03:05 +0800

    chore: update style

--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -3,7 +3,7 @@ import { TbBrandNextjs } from 'react-icons/tb';
 
 export default function Footer() {
   return (
-    <div className="fixed bottom-0 left-0 flex h-7 w-full items-center gap-1 border-t border-border bg-black-300 text-sm z-50">
+    <div className="fixed bottom-0 left-0 z-50 flex h-7 w-full items-center gap-1 border-t border-border bg-black-300 text-sm">
       <div className="flex h-full items-center justify-center bg-primary px-2">
         <VscRemote />
       </div>
@@ -11,8 +11,10 @@ export default function Footer() {
         <VscSourceControl />
         <span>main*</span>
       </div>
-      <div className="flex-1 text-center">© 2024 All Rights Reserved. IRIS YU.</div>
-      <div className="hidden md:flex items-center gap-1 px-2">
+      <div className="flex-1 text-center font-light">
+        © 2024 All Rights Reserved. IRIS YU.
+      </div>
+      <div className="hidden items-center gap-1 px-2 font-light md:flex">
         <TbBrandNextjs />
         <span>Powered by Next.js</span>
       </div>
