2755d9b7e3f401f81bf04023609dc3c116e01efb
Iris Yu <iris.yu0716@gmail.com>
2024-06-20 17:54:34 +0800

    chore: update favicon and intro

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -19,20 +19,34 @@ export default function About() {
       <div>
         <h1 className={className.h1}>
           {`<`}
-          <span className="mx-1">AboutMe</span>
+          <span className="mx-1">關於</span>
           {`/>`}
         </h1>
-        <div className="flex mb-10 gap-4">
+        <div className="mb-10 flex flex-col gap-4">
           <div className="block h-40 w-40 overflow-hidden rounded-full">
             <Image alt="avatar" src="/avatar.png" width={200} height={200} />
           </div>
-          <div>我是 Iris，一個前端工程師，大學非本科系</div>
+          <div>
+            我是 Iris，一個前端工程師，開發上主要使用 React.js 及 Next.js。
+            <br />
+            <br />
+            研究說教導他人是最高效的學習手段，但現實中沒有人好教，因此有了這個部落格的誕生，希望可以透過寫作紀錄的方式，幫助自己學習，若也能幫到需要的人那就太好了！
+            <br />
+            會在這裡分享技術筆記、記錄我的學習歷程，如有錯誤歡迎指教。
+            <br />
+            除了前端外，也會分享一些雜談、我有興趣的事。
+            <br />
+          </div>
+          <div className="text-wrap">
+            <div>我的 Email:</div>
+            <a
+              href="mailto:iris.yu0716@gmail.com"
+              className="ml-2 text-tertiary"
+            >
+              iris.yu0716@gmail.com
+            </a>
+          </div>
         </div>
-        <h1 className={className.h1}>
-          {`<`}
-          <span className="mx-1">AboutThisBlog</span>
-          {`/>`}
-        </h1>
       </div>
     </>
   );
