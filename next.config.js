6ece216149b15822891bde4803c3d0968be84c34
Iris Yu <iris.yu0716@gmail.com>
2024-06-27 14:13:38 +0800

    chore: remove pwa

--- next.config.js
+++ next.config.js
@@ -3,9 +3,4 @@ const nextConfig = {
   reactStrictMode: true,
 };
 
-const withPWA = require("@ducanh2912/next-pwa").default({
-  dest: "public",
-  disable: process.env.NODE_ENV === 'development',
-});
-
-module.exports = withPWA(nextConfig);
+module.exports = nextConfig;
