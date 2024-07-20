62f7a71115c0c6b692cdc52f42fe162a0d5ccf5d
Iris Yu <iris.yu0716@gmail.com>
2024-07-21 00:00:26 +0800

    chore: revert sitemap and update robots.ts

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -5,7 +5,6 @@ export default function robots(): MetadataRoute.Robots {
     rules: {
       userAgent: '*',
       allow: '/',
-      disallow: '/private/',
     },
     sitemap: [`${process.env.NEXT_PUBLIC_HOST}/sitemap.xml`, `${process.env.NEXT_PUBLIC_HOST}/article/sitemap.xml`],
   };
