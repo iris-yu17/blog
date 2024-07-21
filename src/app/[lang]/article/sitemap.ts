c3bb75dd6b8fc25f24db93eaf5f02a3c31fc2f53
Iris Yu <iris.yu0716@gmail.com>
2024-07-21 15:22:09 +0800

    chore: move article siteamp

--- src/app/[lang]/article/sitemap.ts
+++
@@ -1,9 +0,0 @@
-import articles from "@/data/article";
-
-export default async function sitemap() {
-  return articles.map(({ id, updated, priority }) => ({
-    url: `${process.env.NEXT_PUBLIC_HOST}/article/${id}`,
-    lastModified: new Date(updated),
-    priority: priority || 0.5
-  }));
-}
--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,6 +1,12 @@
 import { MetadataRoute } from 'next';
+import articles from "@/data/article";
 
 export default function sitemap(): MetadataRoute.Sitemap {
+  const articlePages = articles.map(({ id, updated, priority }) => ({
+    url: `${process.env.NEXT_PUBLIC_HOST}/article/${id}`,
+    lastModified: new Date(updated),
+    priority: priority || 0.5
+  }));
   return [
     {
       url: `${process.env.NEXT_PUBLIC_HOST}`,
@@ -20,11 +26,6 @@ export default function sitemap(): MetadataRoute.Sitemap {
       changeFrequency: 'weekly',
       priority: 0.5,
     },
-    {
-      url: `${process.env.NEXT_PUBLIC_HOST}/article/sitemap.xml`,
-      lastModified: new Date(),
-      changeFrequency: 'monthly',
-      priority: 0.7,
-    },
+    ...articlePages
   ];
 }
