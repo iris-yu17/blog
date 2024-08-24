ac185291132322123cb69cd1eb8da4286a7d6688
Iris Yu <iris.yu0716@gmail.com>
2024-08-24 18:38:01 +0800

    fix: sitemap issue

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -4,25 +4,25 @@ import articles from "@/data/article";
 export default function sitemap(): MetadataRoute.Sitemap {
   const articlePages = articles.map(({ id, updated, priority }) => ({
     url: `${process.env.NEXT_PUBLIC_HOST}/article/${id}`,
-    lastModified: new Date(updated),
+    lastModified: new Date(updated).toISOString(),
     priority: priority || 0.5
   }));
   return [
     {
       url: `${process.env.NEXT_PUBLIC_HOST}`,
-      lastModified: new Date(),
+      lastModified: new Date().toISOString(),
       changeFrequency: 'weekly',
       priority: 1,
     },
     {
       url: `${process.env.NEXT_PUBLIC_HOST}/about`,
-      lastModified: new Date(),
+      lastModified: new Date().toISOString(),
       changeFrequency: 'monthly',
       priority: 0.8,
     },
     {
       url: `${process.env.NEXT_PUBLIC_HOST}/category`,
-      lastModified: new Date(),
+      lastModified: new Date().toISOString(),
       changeFrequency: 'weekly',
       priority: 0.5,
     },
