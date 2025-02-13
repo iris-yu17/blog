aff2d3e60b542eec200c4d4252f94775562f69df
Iris Yu <irisyu16@oneclass.tw>
2025-02-13 15:15:28 +0800

    feat: update middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,6 +1,6 @@
-import { NextRequest, NextResponse } from "next/server";
-import acceptLanguage from "accept-language";
-import { fallbackLng, languages } from "@/i18n/settings";
+import { NextRequest, NextResponse } from 'next/server';
+import acceptLanguage from 'accept-language';
+import { fallbackLng, languages } from '@/i18n/settings';
 import { i18nRouter } from 'next-i18n-router';
 
 acceptLanguage.languages(languages);
@@ -8,13 +8,13 @@ acceptLanguage.languages(languages);
 export const config = {
   matcher: [
     // skip if match below
-    '/((?!sitemap.xml|robots.txt|markdown|manifest.json|favicon.ico|_next|.*\\.svg$|.*\\.png$).*)',
+    '/((?!sitemap.xml|robots.txt|markdown|manifest.json|ads.txt|favicon.ico|_next|.*\\.svg$|.*\\.png$).*)',
   ],
 };
 
 const i18nConfig = {
   locales: languages,
-  defaultLocale: fallbackLng
+  defaultLocale: fallbackLng,
 };
 
 export function middleware(request: NextRequest) {
